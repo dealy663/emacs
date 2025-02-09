a72d1da8f3f079e8b1f2f2da8662212a4f0b4026
dealy663 <dealy663@gmail.com>
2025-02-09 08:12:24 -0800

    problem with modus theme options

--- custom-vars.el
+++ custom-vars.el
@@ -1,8 +1,8 @@
 ;; manual config file guided from system crafters
 
 ;; Don't display startup message
-(setq inhibit-startup-message t
-      visible-bell t)
+;; (setq inhibit-startup-message t
+;;       visible-bell t)
 
 ;; Turn off some unneeded UI elements
 (menu-bar-mode -1)  ; Leave this one on if you're a beginner!
@@ -25,3 +25,17 @@
 ;; Revert buffers when the underlying file has changed
 (global-auto-revert-mode 1)
 (setq global-auto-revert-non-file-bufferst 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Setup themes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;(require 'modus-themes)
+;;(setq modus-themes-mode-line '(borderless))
+(setq modus-themes-mode-line '(accented padded borderless))
+;;(setq modus-themes-region '(accented))
+(setq modus-themes-completions 'opinionated)
+;;(setq modus-themes-region '(accented))
+(setq modus-themes-region '(bg-only))
+
+;; Theme options must be set before loading the theme
+(load-theme 'modus-vivendi t)
